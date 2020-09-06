c82f378c0a0b4e8a1b8c5f337227e34088c8147e
0kk1n4v4 <70743328+0kk1n4v4@users.noreply.github.com>
2020-09-06 23:12:20 +0300

    v0.12

---
+++ Java-Sber/Home work 1-1/geometry classes/src/test/java/Main.java
@@ -0,0 +1,17 @@
+package test.java;
+/*   (c)   0kk1n4v4 - rnd 06/09/20
+    Не совсем ясное тех. задание.
+    Реализовать иерархию объектов Circle, Rectangle, Triangle, Square
+    Пока что мутим несколько классов с параметрами и методами вычисления
+ */
+
+
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ Java-Sber/Home work 1-1/geometry classes/src/test/java/rectangle.java
@@ -0,0 +1,17 @@
+package test.java;
+
+/* 0kk1n4v4
+https://ru.wikipedia.org/wiki/Прямоугольник
+
+*имеет 4 стороны.
+* есть площадь
+* есть периметр
+
+
+ */
+
+public class rectangle {
+
+
+
+}

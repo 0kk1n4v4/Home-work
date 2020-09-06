76910e08ee3f51dff59f5eeca1fc3010ddad1234
0kk1n4v4 <70743328+0kk1n4v4@users.noreply.github.com>
2020-09-06 22:14:56 +0300

    v0.11

--- Java-Sber/Home work 1-1/array bublle sort/src/test/java/Main.java
+++ Java-Sber/Home work 1-1/array bublle sort/src/test/java/Main.java
@@ -1,13 +1,26 @@
 package test.java;
 
+/* (c) okkinava - rnd 06/09/20
+   array bublle sorter
+* */
+import java.io.*;
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-        int[] array1 = new int [5];
-        for (int i = 0; i < 5; i++) {
-            array1[i] = (int) Math.random();
-            System.out.println(array1[i]);
+    public static void main(String[] args) throws IOException {
+        //Создаем ридер
+        BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
+        //
+        Integer y = Integer.parseInt(reader.readLine());
+
+
+
+	// Создаем и заполняем массив случайными числам
+        int[] array1 = new int [y]; // Массив из х элементов
+        for (int i = 0; i < y; i++) {
+            int x = (int) (Math.random() * 9);
+            array1[i] = x;
+            System.out.print(array1[i]);
+
         }
 
     }

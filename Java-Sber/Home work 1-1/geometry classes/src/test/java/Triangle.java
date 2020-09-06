13f4be1b574f3ae9e4b3f63098eeca257224b27b
0kk1n4v4 <70743328+0kk1n4v4@users.noreply.github.com>
2020-09-07 00:10:53 +0300

    v0.12

--- Java-Sber/Home work 1-1/geometry classes/src/test/java/Triangle.java
+++ Java-Sber/Home work 1-1/geometry classes/src/test/java/Triangle.java
@@ -1,7 +1,22 @@
 package test.java;
 
+/*
+https://ru.wikipedia.org/wiki/Прямоугольный_треугольник
+
+Подразумеваем что у нас прямоугольный треугольник.
+
+* вычисляем площадь
+* катеты и гипотенузу
+
+ */
+
 public class Triangle {
 
     int a,b,c;
-    
+
+    public static int trian( int a, int b, int c){
+
+
+    }
+
 }

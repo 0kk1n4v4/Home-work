055569175a19ca3590d0fe0835c3eda5f7848858
0kk1n4v4 <70743328+0kk1n4v4@users.noreply.github.com>
2020-09-06 21:57:05 +0300

    v0.11

--- Java-Sber/Home work 1-1/array bublle sort/src/test/java/Main.java
+++ Java-Sber/Home work 1-1/array bublle sort/src/test/java/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int[] array1 = new int [5];
+        for (int i = 0; i < 5; i++) {
+            array1[i] = (int) Math.random();
+            System.out.println(array1[i]);
+        }
+
     }
 }

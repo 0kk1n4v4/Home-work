53c8f8e30160d24c94ff213557712b70676448a7
0kk1n4v4 <70743328+0kk1n4v4@users.noreply.github.com>
2020-09-10 12:00:06 +0300

    v0.2

--- Java-Sber/Home work 1-2/task 2/src/test/java/main.java
+++ Java-Sber/Home work 1-2/task 2/src/test/java/main.java
@@ -1,31 +1,53 @@
 package test.java;
+/*
+    0kk1n4v4 rnd /09/09/20
 
-import java.util.Map;
-import java.util.Map;
-
-import java.util.HashMap;
-
-public static void main(String[]args){
-
-        Map<String, String> myMap = new HashMap<>();
-        myMap.put("d", "D");
-        myMap.put("c", "C");
-        myMap.put("b", "B");
-        myMap.put("A", "A");
-        List<String> myList = Arrays.asList("a", "b", "c");
-
-        Function<Entry<String, String> , Boolean> myCondition =  i -> myList.contains(i.getKey());
-
-        Map<Boolean,List<Entry<String, String>>>  myPartedMap = myMap.entrySet()
-        .stream().collect(Collectors.groupingBy(myCondition));
-
-        System.out.println(myPartedMap);
+ */
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.function.Consumer;
 
 
+class Main {
+
+    // 1 часть подсчет уникальных слов в файле
+    public static void main(String[] args) throws Exception {
+        File file = new File("C:\\temp\\test.txt");
+        BufferedReader filereader = new BufferedReader(new FileReader(file));
+        String linefromfile = filereader.readLine();
+        Map<String, Integer> tempmap = new HashMap<String, Integer>();
+
+        while ((linefromfile != null)) {
+            String[] singlewords = linefromfile.split(" ");
+            for (String word : singlewords) {
+                tempmap.put(word, word.hashCode());
+                linefromfile = filereader.readLine();
+            }
+        }
+        System.out.println("Количество уникальных элементов :" + tempmap.size());
+
+        // 4 часть вывода файла наоборот
+        System.out.println("Выводим файл в обратном порядке :");
+
+        List<String> list = new ArrayList<String>();
+        Scanner in = new Scanner(new File("C:\\temp\\test.txt"));
+        while (in.hasNextLine()) {
+            list.add(in.nextLine());
+        }
+        for (int i = list.size()-1; i >=0; i--) {
+            System.out.println(list.get(i));
         }
 
 
 
+
+
+    }
+
+
+
+}
 
 
 
